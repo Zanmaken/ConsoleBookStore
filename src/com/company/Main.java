7e8b79eaf43d279989fabe764a764223452bf6c8
zanmaken <rafal.kryczyk@outlook.com>
2022-03-15 17:10:28 +0100

    Added a way for the user to log out when in the main menu.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     static ArrayList<String> menuButtons = new ArrayList<>(Arrays.asList("Create account", "Login", "Search for books", "Exit program"));
     static ArrayList<String> menuButtonsLoggedIn = new ArrayList<>(Arrays.asList("Search for Books", "Order History", "Shopping Cart", "Account Settings", "Log out", "Exit program"));
@@ -13,13 +13,13 @@ public class Main{
     static ArrayList<String> searchBooksButtons = new ArrayList<>(Arrays.asList("Search by name", "Search by category", "Search by author"));
     public static User currentUser = null;
 
-    public static void main(String[] args){
-       mainMenu();
+    public static void main(String[] args) {
+        mainMenu();
     }
 
-    private static void mainMenu(){
+    private static void mainMenu() {
         mainMenuButtonsPrint();
-        switch(checkUserLoginStatus()){
+        switch (checkUserLoginStatus()) {
             case 0:
                 mainMenuChoiceNotLogged();
                 break;
@@ -32,13 +32,13 @@ public class Main{
         }
     }
 
-    private static void printButtons(ArrayList<String> buttonList){  // Prints menu buttons from list to the console when called
-        for(int x = 1; x <= buttonList.size(); x++)
+    private static void printButtons(ArrayList<String> buttonList) {  // Prints menu buttons from list to the console when called
+        for (int x = 1; x <= buttonList.size(); x++)
             System.out.println(x + "." + buttonList.get(x - 1));
     }
 
-    private static void mainMenuButtonsPrint(){          // Uses correct printButton() method,
-        switch(checkUserLoginStatus()){             // depending on the login status of the current user.
+    private static void mainMenuButtonsPrint() {          // Uses correct printButton() method,
+        switch (checkUserLoginStatus()) {             // depending on the login status of the current user.
             case 0:
                 printButtons(menuButtons);
                 break;
@@ -52,8 +52,8 @@ public class Main{
         System.out.println("Choose one of the options(Type the number): ");
     }
 
-    private static void mainMenuChoiceNotLogged(){
-        switch(userInputInt(menuButtons.size())){
+    private static void mainMenuChoiceNotLogged() {
+        switch (userInputInt(menuButtons.size())) {
             case 1:
                 currentUser = new User();
                 currentUser.saveUser();
@@ -71,8 +71,8 @@ public class Main{
         }
     }
 
-    private static void mainMenuChoiceLoggedIn(){
-        switch(userInputInt(menuButtonsLoggedIn.size())){
+    private static void mainMenuChoiceLoggedIn() {
+        switch (userInputInt(menuButtonsLoggedIn.size())) {
             case 1:
                 searchForBooksMenu();
                 break;
@@ -86,15 +86,15 @@ public class Main{
                 System.out.println("--Account settings method placeholder--");
                 break;
             case 5:
-                System.out.println("--Log out method placeholder--");
+                logOut();
                 break;
             case 6:
                 break;
         }
     }
 
-    private static void mainMenuChoiceAdmin(){
-        switch(userInputInt(menuButtonsAdmin.size())){
+    private static void mainMenuChoiceAdmin() {
+        switch (userInputInt(menuButtonsAdmin.size())) {
             case 1:
                 searchForBooksMenu();
                 break;
@@ -108,62 +108,66 @@ public class Main{
                 System.out.println("--Account settings method placeholder--");
                 break;
             case 5:
-                System.out.println("--Log out method placeholder--");
+                logOut();
                 break;
             case 6:
                 break;
         }
     }
 
-    private static int checkUserLoginStatus(){          // Checks if the user is logged in and has admin privileges
-        if(currentUser != null && currentUser.isAdmin)  // then returns 0 for not logged in, 1 for logged in
+    private static void logOut() {
+        currentUser = null;
+        mainMenu();
+    }
+    private static int checkUserLoginStatus() {          // Checks if the user is logged in and has admin privileges
+        if (currentUser != null && currentUser.isAdmin)  // then returns 0 for not logged in, 1 for logged in
             return 2;                                   // and 2 for logged in with admin privileges.
-        else if(currentUser != null)
+        else if (currentUser != null)
             return 1;
         else
             return 0;
     }
 
 
-    private static void searchForBooksMenu(){
+    private static void searchForBooksMenu() {
         int x = 1;
-        for(String button : searchBooksButtons){
+        for (String button : searchBooksButtons) {
             System.out.println(x + "." + button);
             x++;
         }
         System.out.println("Choose one of the options above(Type in the number): ");
-        while(true){
+        while (true) {
             Scanner uInput = new Scanner(System.in);
             int userInput = uInput.nextInt();
-            if(userInput > searchBooksButtons.size() || userInput < 1){
+            if (userInput > searchBooksButtons.size() || userInput < 1) {
                 System.out.println("Number is out of bounds, please try again.");
                 continue;
             }
-            switch(userInput){
-                case 1:{
+            switch (userInput) {
+                case 1: {
                     searchBookByName();
                 }
             }
         }
     }
 
-    private static void searchBookByName(){
+    private static void searchBookByName() {
         System.out.print("Type the name of the book: ");
         Scanner uInput = new Scanner(System.in);
         String userInput = uInput.nextLine();
     }
 
-    private static int userInputInt(int listLength){  // Gets the user's input for selecting menu buttons
-       Scanner uInput = new Scanner(System.in);       // and returns the choice as an int.
-       int userInput = uInput.nextInt();
-       while(userInput < 1 || userInput > listLength){
-           System.out.print("Number is out of bounds, please enter a number from 1 to " + listLength + ": ");
-           userInput = uInput.nextInt();
-       }
-       return userInput;
+    private static int userInputInt(int listLength) {  // Gets the user's input for selecting menu buttons
+        Scanner uInput = new Scanner(System.in);       // and returns the choice as an int.
+        int userInput = uInput.nextInt();
+        while (userInput < 1 || userInput > listLength) {
+            System.out.print("Number is out of bounds, please enter a number from 1 to " + listLength + ": ");
+            userInput = uInput.nextInt();
+        }
+        return userInput;
     }
 
-    private static String userInputAsString(){
+    private static String userInputAsString() {
         Scanner uInput = new Scanner(System.in);
         return uInput.nextLine();
     }
